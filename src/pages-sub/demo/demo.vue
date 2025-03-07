f1bec3deb2575fe0e74303a9e385cf858bfac69b
tianguoyuan <603660895@qq.com>
2025-03-07 15:37:14 +0800

    plugin svgo

--- src/pages-sub/demo/demo.vue
+++ src/pages-sub/demo/demo.vue
@@ -1,7 +1,8 @@
 <script lang="ts" setup>
+import { useUserStore } from '@/store'
 import PLATFORM from '@/utils/platform'
 
-//
+const userStore = useUserStore()
 function back() {
   uni.reLaunch({ url: '/pages/index/index' })
 }
@@ -13,6 +14,7 @@ function pageToList() {
 }
 
 function removeStorage() {
+  userStore.clearUserInfo()
   uni.clearStorageSync()
 }
 </script>
@@ -24,7 +26,9 @@ function removeStorage() {
     <view class="mt-3">
       <wd-button @click="pageToList">PageTo-List</wd-button>
     </view>
-    <wd-button class="block mt-3" @click="removeStorage">删除storage</wd-button>
+    <view class="mt-3">
+      <wd-button class="block mt-3" @click="removeStorage">删除storage</wd-button>
+    </view>
   </view>
 </template>
 
